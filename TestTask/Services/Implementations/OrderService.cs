e1cb9e8d4185e148db6781ed72e2677e2d768b84
Vitalii Murashko <jolly0872@gmail.com>
2024-11-14 21:24:59 +0300

    GetOrder implemented

--- TestTask/Services/Implementations/OrderService.cs
+++ TestTask/Services/Implementations/OrderService.cs
@@ -1,13 +1,31 @@
-﻿using TestTask.Models;
+﻿using TestTask.Data;
+using TestTask.Models;
 using TestTask.Services.Interfaces;
 
 namespace TestTask.Services.Implementations
 {
+
     public class OrderService : IOrderService
     {
-        public Task<Order> GetOrder()
+        private readonly ApplicationDbContext _appDbContext;
+
+        public OrderService(ApplicationDbContext appDbContext)
         {
-            throw new NotImplementedException();
+            _appDbContext = appDbContext;
+        }
+
+        /// <summary>
+        /// Newest order with more than one item
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<Order> GetOrder()
+        {
+            var result =  _appDbContext.Orders
+                .Where(x => x.Quantity > 1)
+                .OrderByDescending(x => x.CreatedAt)
+                .First();
+            return result;
         }
 
         public Task<List<Order>> GetOrders()
